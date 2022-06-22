b7a9099d068f2a2a8fab2e977c7915ed0be31f1d
0S-Car <oscarthapa77@gmail.com>
2022-06-22 14:23:58 +0545

    hoisting, coersion,arrow function, etc concepts

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 
 const pi = 3.1415
 let a = 1
+let b = 3
 
 console.log(a)
 
+function addNum (a,b){
+    return a+b
+}
+console.log(addNum(2,3))
+
+const subNum = (a,b) => a-b 
+console.log(subNum(5,4))
