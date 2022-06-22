94bb731bb46a81809139e2e85ed7ab359923819e
0S-Car <oscarthapa77@gmail.com>
2022-06-22 13:05:29 +0545

    do js

---
+++ index.js
@@ -0,0 +1,9 @@
+//hoisting is the process in which the interpreter reads the declaration (variable, function)
+// var shows hoisting... So, BAD!!
+//JS is dynamically typed, no need to declare the type of variable
+
+const pi = 3.1415
+let a = 1
+
+console.log(a)
+
