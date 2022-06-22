034e87e98138a000d7dd1ea6900d96490f63b943
0S-Car <oscarthapa77@gmail.com>
2022-06-22 15:53:52 +0545

    learn new

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // var shows hoisting... So, BAD!!
 //JS is dynamically typed, no need to declare the type of variable
 
-const pi = 3.1415
-let a = 1
-
-console.log(a)
-
+const obj = {
+    name : 'Oscar',
+    grade : 12,
+    isDead : false,
+}
+alert("Hello")
+prompt("Hoi!!")
+console.log("OOI!")
